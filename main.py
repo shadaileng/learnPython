f2f2303a703c3a3f2a769488799bbea8ec1cd23f
shadaileng <qpf0510@qq.com>
2017-12-06 17:39:57 +0800

    datetime

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ __author__ = 'Shadaileng'
 
 from process_test import ufork, multiProcess, processPool,subProcess,queuerw, thread
 
+from datetime import datetime
+
 if __name__ == '__main__':
 	print(__doc__  % __author__)
 	print('********IO************')
@@ -29,4 +31,17 @@ if __name__ == '__main__':
 	
 	#queuerw()
 	
-	thread()
+	#thread()
+	
+	now = datetime.now()
+	print('now:', now, now.timestamp())
+	
+	custom = datetime(2020, 12, 25, 5, 5)
+	custom_t = custom.timestamp()
+	
+	print('origin:', custom, '\ntimestamp:', custom_t, '\nfromtimestamp:', datetime.fromtimestamp(custom_t), '\nutcfromtimestamp', datetime.utcfromtimestamp(custom_t))
+	
+	strtime = datetime.strptime('2017-12-25 05:05:00', '%Y-%m-%d %H:%M:%S')
+	
+	print('str2time:', strtime)
+	print('time2str:', strtime.strftime('%Y-%m-%d %H:%M:%S'))
